b0901f0de0b3b50500fa2ac6235024080ba340ec
KyleMit <kmitofsky@stackoverflow.com>
2022-07-11 08:51:13 -0400

    add md preview

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,30 @@
 const { minify } = require("terser")
 const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
 
     // add assets
-    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    const md = markdownIt({
+      html: true,
+      breaks: true,
+      linkify: true
+    })
+    eleventyConfig.setLibrary("md", md);
+
+    eleventyConfig.addFilter("md", function(code) {
+      // return original text if run in dev
+      return md.render(code);
+    });
+
+    eleventyConfig.addFilter("highlight", function(code) {
+      // return original text if run in dev
+      return `<pre>${code}</pre>`;
+    });
 
 
     eleventyConfig.addFilter("cssmin", function(code) {
@@ -14,7 +32,6 @@ module.exports = function(eleventyConfig) {
         if (process.env.ELEVENTY_ENV.toLowerCase() == "dev") return code
         return new CleanCSS({}).minify(code).styles;
     });
-
 
     eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
         // return original text if run in dev
@@ -40,6 +57,5 @@ module.exports = function(eleventyConfig) {
             layouts: "_layouts"
         },
 
-        markdownTemplateEngine: "njk",
     };
 };
--- ReadMe.md
+++ ReadMe.md
@@ -8,7 +8,12 @@ Personal bio and some links to other places I exist on the interwebs
 * add md filter - also generate preview
 * enable preview toggle
 * add line numbers
-* add syntax highlighting colors
 * add line highlight?
   * process markdown and break up into divs?
-
+* regular markdown
+  * Read as text
+    * text -> syntax highlighting -> (line nums)
+    * text -> markdown render
+  * Read as fenced code block
+    * syntax highlighting
+    * highlighted -> text content -> markdown render
